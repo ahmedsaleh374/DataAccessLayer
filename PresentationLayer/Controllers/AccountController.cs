9b44e8f3ef0a712ce7bd8dcaf9ba725b6bf6ab2e
Ahmed Saleh <ahmedsaleh54007@gmail.com>
2025-07-27 21:44:39 +0300

    new feature forget password

--- PresentationLayer/Controllers/AccountController.cs
+++ PresentationLayer/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using PresentationLayer.Helpers;
 using PresentationLayer.ViewModels;
 using System.Threading.Tasks;
 
@@ -87,10 +88,79 @@ namespace PresentationLayer.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> SignOut() 
+        public async Task<IActionResult> SignOut()
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction(nameof(Login));
         }
+
+        [HttpGet]
+        public IActionResult ForgetPassword() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> SendResetPasswordLink(ForgetPasswordVM input)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(input.Email);
+                if (user is not null)
+                {
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                    var passwordLink = Url.Action(nameof(ResetPassword), "Account",
+                        new { Email = input.Email, Token = token }, Request.Scheme);
+
+                    var email = new Email
+                    {
+                        to = input.Email,
+                        subject = "Reset Password",
+                        body = passwordLink
+                    };
+                    EmailSettings.SendEmail(email);
+                    return RedirectToAction(nameof(CheckYourInbox));
+                }
+            }
+            ModelState.AddModelError(string.Empty, "invalid options");
+            return View(nameof(ForgetPassword), input);
+        }
+
+
+        public IActionResult CheckYourInbox()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword(string Email, string Token)
+        {
+            TempData["Email"] = Email;
+            TempData["Token"] = Token;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordVM input)
+        {
+            if (ModelState.IsValid) 
+            {
+                string email = TempData["Email"] as string ?? "";
+                string token = TempData["Token"] as string ?? "";
+
+                var user = await _userManager.FindByEmailAsync(email);
+                if (user != null)
+                {
+                    var result = await _userManager.ResetPasswordAsync(user, token, input.Password);
+                    if (result.Succeeded)
+                        return RedirectToAction(nameof(Login));
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(nameof(ResetPassword), input);
+        }
+
+
     }
 }
---
+++ PresentationLayer/Helpers/Email.cs
@@ -0,0 +1,11 @@
+﻿namespace PresentationLayer.Helpers
+{
+    public class Email
+    {
+        public string to { get; set; }
+
+        public string subject { get; set; }
+
+        public string body { get; set; }
+    }
+}
